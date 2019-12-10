d53bf84b5ffe3057e4b75aecb9016410dec79350
johnsonj561 <johnsonj561@gmail.com>
2019-12-10 15:24:21 -0500

    check Input prop types

---
+++ jotto/src/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = (props) => {
+  const { secretWord } = props;
+  return (
+    <div data-test="component-input" />
+  );
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+}
+export default Input;
---
+++ jotto/src/Input.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../test/testUtils';
+import Input from './Input'
+
+const defaultProps = {
+  secretWord: 'party',
+};
+
+const setup = (props={}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input{...setupProps} />)
+}
+
+it('renders without crashing', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-input');
+  expect(component.length).toBe(1);
+})
+
+it('throws no warning with expected prop types', () => {
+  const propError = checkProps(Input, defaultProps);
+  expect(propError).toBeUndefined();
+})
